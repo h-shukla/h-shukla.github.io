d23778600092d317f443294e5de16f8e46a693aa
Himanshu Shukla <hshukla2611@gmail.com>
2025-06-02 03:29:50 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <div className="bg-gray-800 min-h-screen">
+            <h1 className="text-2xl underline text-slate-500">
+                Hello, World!!
+            </h1>
+        </div>
+    );
+};
+
+export default App;
